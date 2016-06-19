717e48deb566edfe0bf5425b5200d6f5a8522817
PabloEscobar13 <thesavetrees@gmail.com>
2016-06-19 10:45:55 +0200

    This is the diffrent version of my app I'm quite not sure how should I store the date.

---
+++ src/com/pablo/Main.java
@@ -0,0 +1,108 @@
+package com.pablo;
+
+import com.pablo.com.info.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Dish dish1 = new PolishFood("Pierogi", 10.60);
+//        Dish dish2 = new PolishFood("Bigos", 11.20);
+//        Dish dish3 = new PolishFood("Schabowy", 16.00);
+//        Dish dish4 = new ItalianFood("Pizza", 12.50);
+//        Dish dish5 = new ItalianFood("Spaghetti", 13.90);
+//        Dish dish6 = new ItalianFood("Lazagne", 17.30);
+//        Dish dish7 = new MexicanFood("Buritto", 13.10);
+//        Dish dish8 = new MexicanFood("Tortilla", 11.70);
+//        Dish dish9 = new MexicanFood("Tortilla1", 14.10);
+//        Dish dish10 = new Drink("Mohiato", 11.00);
+//        Dish dish11 = new Drink("SexOnTheBeach", 14.60);
+//        Dish dish12 = new Drink("Vodka", 8.70);
+
+        boolean somethingElse = false;
+        ArrayList<Dish> orderList = new ArrayList<>(2);
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hello can I take your order?");
+
+        do {
+            System.out.println("Would you like a lunch or a drink?");
+            System.out.println(Dish.getOffers());
+            String meal = scan.next();
+            switch (meal) {
+
+                /**Client order a lunch BEGIN**/
+                case "lunch": {
+                    System.out.println("What cuisine do you prefer?");
+                    System.out.println(Dish.getStringList(Dish.getCuisines()));
+                    meal = scan.next();
+                    switch (meal) {
+                        case "Polish":{
+                            Dish.askForOrder(PolishFood.getPolishDish());
+                            String order = scan.next();
+                            for (Dish d: PolishFood.getPolishDish()){
+                             if (d.getName().equals(order)){
+                                 orderList.add(d);
+                                 break;
+                             }
+                            }
+                            System.out.println("Anything else? ");
+                        }
+                        case "Mexico":{
+                            Dish.askForOrder(MexicanFood.getMexicanDish());
+                            String order = scan.next();
+                            for (Dish d: MexicanFood.getMexicanDish()){
+                                if (d.getName().equals(order)){
+                                    orderList.add(d);
+                                    break;
+                                }
+                            }
+                            System.out.println("Anything else? ");
+                        }
+                        case "Italian":{
+                            Dish.askForOrder(ItalianFood.getItalianDish());
+                            String order = scan.next();
+                            for (Dish d: ItalianFood.getItalianDish()){
+                                if (d.getName().equals(order)){
+                                    orderList.add(d);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }/**Client order a lunch END**/
+
+                /**Client order a drink BEGIN**/
+                case "drink": {
+                    System.out.println("What drink do you prefer?");
+                }
+                /**Client order a drink END**/
+            }
+            int check = -1;
+            System.out.println("Anything else? ");
+            do {
+                System.out.println("yes/no");
+                String yesNo;
+                yesNo = scan.next();
+                if (yesNo.equals("yes")) {
+                    somethingElse = true;
+                    check = 1;
+
+                } else if (yesNo.equals("no")) {
+                    somethingElse = false;
+                    check = 1;
+                } else {
+                    System.out.println("Sorry sir?");
+                }
+            } while (check == -1);
+        }while (somethingElse) ;
+
+        System.out.println(Arrays.toString(orderList.toArray()));
+    }
+}
+
